749c306c28ada78b08200b32e07602a0a9c9c8a4
Kyle Neale <kyle.a.neale@gmail.com>
2018-05-31 20:45:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, intruments) {
+  var newArr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var artist = musicians[i];
+    var instrument = instruments[i];
+    newArr.push(`${artist} plays ${instrument}`);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(facts) {
+  var newArr = [];
+  var i = 0
+  while(i < facts.length) {
+    var fact = facts[i];
+    facts[i] = fact + '!!!'
+    i++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles() {
+  
+}
